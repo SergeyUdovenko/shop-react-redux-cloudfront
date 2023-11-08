0a061de63741d20b53d86c47114023ae887579d9
Serhii-Udovenko <serhii.udovenko@xa.epicgames.com>
2023-11-08 21:25:34 +0100

    add auth

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   products: "https://kwe5ktkkb9.execute-api.eu-central-1.amazonaws.com/",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev"
+  order: "https://kwe5ktkkb9.execute-api.eu-central-1.amazonaws.com/",
+  import: "https://tfeqr2qy5m.execute-api.eu-central-1.amazonaws.com/",
+  bff: "https://tfeqr2qy5m.execute-api.eu-central-1.amazonaws.com/",
+  cart: "https://kwe5ktkkb9.execute-api.eu-central-1.amazonaws.com/"
 };
 
 export default API_PATHS;
