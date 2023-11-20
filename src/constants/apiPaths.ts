c25222f6bf6799ea21574d83ca1bc489a3c3ffab
Serhii-Udovenko <serhii.udovenko@xa.epicgames.com>
2023-11-20 21:13:57 +0100

    task-8

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://kwe5ktkkb9.execute-api.eu-central-1.amazonaws.com/",
   import: "https://tfeqr2qy5m.execute-api.eu-central-1.amazonaws.com/",
   bff: "https://tfeqr2qy5m.execute-api.eu-central-1.amazonaws.com/",
-  cart: "https://kwe5ktkkb9.execute-api.eu-central-1.amazonaws.com/"
+  cart: "https://6go1rd11wi.execute-api.eu-central-1.amazonaws.com/dev/"
 };
 
 export default API_PATHS;
